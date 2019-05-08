781e4dcb307c17f6ef9df8fb63f889011ac21719
anonymous <anonymous@xyz.com>
2019-05-08 20:59:32 +0200

    duplicate code extracted as method pinsDownInEveryRow

--- app/src/test/java/com/kata/bowlinggame/model/BowlingGameTest.java
+++ app/src/test/java/com/kata/bowlinggame/model/BowlingGameTest.java
@@ -1,30 +1,36 @@
 package com.kata.bowlinggame.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
 
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setUp() {
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
     public void shouldReturnScoreAs0_WhenNoPinsDownInAll20Rolls() {
-        BowlingGame bowlingGame = new BowlingGame();
-
-        for (int roll = 0; roll < 20; roll++) {
-            bowlingGame.pins(0);
-        }
+        pinsDownInEveryRow(0);
 
         assertEquals(0, bowlingGame.score().intValue());
     }
 
     @Test
     public void shouldReturnScoreAs20_When1PinDownInAll20Rolls() {
-        BowlingGame bowlingGame = new BowlingGame();
-
-        for (int roll = 0; roll < 20; roll++) {
-            bowlingGame.pins(1);
-        }
+        pinsDownInEveryRow(1);
 
         assertEquals(20, bowlingGame.score().intValue());
+    }
+
+    private void pinsDownInEveryRow(int pinsDown) {
+        for (int roll = 0; roll < 20; roll++) {
+            bowlingGame.pins(pinsDown);
+        }
     }
 }
